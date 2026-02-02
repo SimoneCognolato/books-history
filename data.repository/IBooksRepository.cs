2702ef18820724836df239fea8c37ed597710f89
Simone Cognolato <simone.cognolato@backbone.one>
2026-02-02 18:19:26 +0100

    feat(db): add repository

---
+++ data.repository/IBooksRepository.cs
@@ -0,0 +1,16 @@
+﻿using data.model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data.repository
+{
+    public interface IBooksRepository
+    {
+        Task<List<Book>> GetAll();
+        Task<Book?> GetById(int id);
+        Task<bool> Add(Book book);
+    }
+}
---
+++ data.repository/InMemoryBooksRepository.cs
@@ -0,0 +1,45 @@
+﻿using data.migration;
+using data.model.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data.repository
+{
+    public class InMemoryBooksRepository : IBooksRepository
+    {
+        private readonly BooksDbContext _dbContext;
+
+        public InMemoryBooksRepository(BooksDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Add(Book book)
+        {
+            var existingBook = await _dbContext.Books.FirstOrDefaultAsync(u => u.Id == book.Id);
+
+            if (existingBook != null)
+            {
+                await _dbContext.Books.AddAsync(book);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<List<Book>> GetAll()
+        {
+            return await _dbContext.Books.ToListAsync();
+        }
+
+        public async Task<Book?> GetById(int id)
+        {
+            return await _dbContext.Books.FirstOrDefaultAsync(u => u.Id == id);
+        }
+    }
+}
