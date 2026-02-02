a9c89bb53fb935875a57de5a62d6b1e3c65b5dfe
Simone Cognolato <simone.cognolato@backbone.one>
2026-02-02 18:19:46 +0100

    feat(db): add mapping of bookDTO to book

---
+++ books-history/MappingProfiles/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using api.model.DTOs;
+using AutoMapper;
+using data.model.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace books_history.MappingProfiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() {
+            CreateMap<BookDTO, Book>().ReverseMap();
+        }
+    }
+}
