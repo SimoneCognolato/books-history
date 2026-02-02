e0b6f208edcbef666eeac913881343bfca687dfe
Simone Cognolato <simone.cognolato@backbone.one>
2026-02-02 16:50:53 +0100

    feat(books): add initial books controller

---
+++ books-history/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using api.model.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace books_history.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly ILogger<BooksController> _logger;
+
+        public BooksController(ILogger<BooksController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Task<ActionResult<List<BookDTO>>> GetBooks()
+        {
+            return null;
+        }
+
+    }
+}
