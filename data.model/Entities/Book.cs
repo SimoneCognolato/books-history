798e0036844fc68fa90f931603d6fb02060cb36d
Simone Cognolato <simone.cognolato@backbone.one>
2026-02-02 18:18:55 +0100

    feat(db): add book data model

---
+++ data.model/Entities/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data.model.Entities
+{
+    public class Book
+    {
+        public int EntityId { get; set; }
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public DateTime PublishDate { get; set; }
+        public required List<string> Authors { get; set; }
+    }
+}
